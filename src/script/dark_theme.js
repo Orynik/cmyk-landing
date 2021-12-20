936603b296ec4840a59ba7caef73b38be6fde62e
Orynik <dkrovel@gmail.com>
2021-12-20 21:43:42 +0800

    Added adaptive on 1440 for last section Added night theme

---
+++ src/script/dark_theme.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let darkItemSwitcher = document.getElementById('theme-swicher')
+
+let isSwitchOn = false;
+
+darkItemSwitcher.addEventListener('click',(event) => {
+  if(isSwitchOn){
+    event.target.src = 'src/assets/sun.svg'
+    document.documentElement.style.setProperty('--dark-theme','#252525')
+    document.documentElement.style.setProperty('--dark-theme__text','#fff')
+  }else{
+    event.target.src = 'src/assets/moon.svg'
+    document.documentElement.style.setProperty('--dark-theme','#fff')
+    document.documentElement.style.setProperty('--dark-theme__text','#252525')
+  }
+  isSwitchOn = !isSwitchOn;
+})
