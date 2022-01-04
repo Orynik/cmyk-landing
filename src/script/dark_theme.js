83252383bf473adca2bb2d61df7fad29aaf77928
Orynik <dkrovel@gmail.com>
2022-01-05 01:58:54 +0800

    hot fix: * burger icon on lg size * change images

--- src/script/dark_theme.js
+++ src/script/dark_theme.js
@@ -3,19 +3,18 @@
 let darkItemSwitcher1 = document.getElementById('theme-swicher-1')
 let darkItemSwitcher2 = document.getElementById('theme-swicher-2')
 
-let isSwitchOn = false;
+let isSwitchOn = true;
 
 darkItemSwitcher1.addEventListener('click',switchTheme)
-
 darkItemSwitcher2.addEventListener('click',switchTheme)
 
 function switchTheme(event){
   if(isSwitchOn){
-    event.target.src = 'src/assets/sun.svg'
+    event.target.src = 'src/assets/sun.png'
     document.documentElement.style.setProperty('--dark-theme','#252525')
     document.documentElement.style.setProperty('--dark-theme__text','#fff')
   }else{
-    event.target.src = 'src/assets/moon.svg'
+    event.target.src = 'src/assets/night.png'
     document.documentElement.style.setProperty('--dark-theme','#fff')
     document.documentElement.style.setProperty('--dark-theme__text','#252525')
   }
